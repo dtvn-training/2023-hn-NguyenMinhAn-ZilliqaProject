900df1b8a67d577c68901947a638bfe3db1b9bbb
KawaiiHasuu <annm01032002@gmail.com>
2023-11-29 10:18:04 +0700

    First Commit

---
+++ zilliqa_project/src/main/java/com/annm/zilliqa_project/ZilliqaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.annm.zilliqa_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZilliqaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZilliqaProjectApplication.class, args);
+	}
+
+}
---
+++ zilliqa_project/src/test/java/com/annm/zilliqa_project/ZilliqaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annm.zilliqa_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZilliqaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
