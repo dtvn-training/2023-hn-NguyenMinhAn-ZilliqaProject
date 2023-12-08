f9344d7cf6af13bfd87f30c91750cb643b3e51fe
KawaiiHasuu <annm01032002@gmail.com>
2023-12-08 17:05:38 +0700

    use security

---
+++ zilliqa_project/src/main/java/com/annm/zilliqa_project/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.annm.zilliqa_project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsServiceCustom();
+    }
+}
