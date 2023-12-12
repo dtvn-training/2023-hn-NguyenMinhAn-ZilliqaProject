395b087ff19bfcad61992de99aead4836afd56a9
KawaiiHasuu <annm01032002@gmail.com>
2023-12-12 17:12:54 +0700

    Add login, register,...

--- zilliqa_project/src/main/java/com/annm/zilliqa_project/web/RegisterUser.java
+++ zilliqa_project/src/main/java/com/annm/zilliqa_project/web/RegisterUser.java
@@ -15,8 +15,71 @@ public class RegisterUser {
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[@#$%^&+=!])(?=\\S+$).*$",
             message = "Mật khẩu phải chứa ít nhất 1 chữ số và 1 ký tự đặc biệt")
     private String password;
+
+    private String repeatPassword;
     private String firstName;
     private String lastName;
 
 
+    @Override
+    public String toString() {
+        return "RegisterUser{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", repeatPassword='" + repeatPassword + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public RegisterUser() {
+    }
+
+    public RegisterUser(String username, String password, String repeatPassword, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.repeatPassword = repeatPassword;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
