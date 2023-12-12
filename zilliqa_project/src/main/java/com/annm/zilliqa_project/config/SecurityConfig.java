6a29d8827a7430a75093ff003687713056cb87d9
KawaiiHasuu <annm01032002@gmail.com>
2023-12-12 10:54:01 +0700

    Config security

--- zilliqa_project/src/main/java/com/annm/zilliqa_project/config/SecurityConfig.java
+++ zilliqa_project/src/main/java/com/annm/zilliqa_project/config/SecurityConfig.java
@@ -1,21 +1,45 @@
 package com.annm.zilliqa_project.config;
 
+import com.annm.zilliqa_project.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        return new UserDetailsServiceCustom();
+    public DaoAuthenticationProvider authenticationProvider(UserService userService){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(
+                configurer->configurer
+                        .anyRequest().authenticated()
+        ).formLogin(
+                form->form.loginPage("/showLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll()
+        ).logout(
+                logout->logout.permitAll()
+        ).exceptionHandling(
+                configurer->configurer.accessDeniedPage("/showPage404")
+        );
+
+        return http.build();
     }
 }
--- zilliqa_project/src/main/java/com/annm/zilliqa_project/entity/Users.java
+++ zilliqa_project/src/main/java/com/annm/zilliqa_project/entity/Users.java
@@ -9,32 +9,109 @@ import javax.validation.constraints.Size;
 import java.util.Collection;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "users")
 public class Users {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
+    @Column(name="id")
     private Long id;
-
-    @Size(min = 3, max = 15, message = "Tên cần trong khoảng 3-15 kí tự")
-    @Column(name = "first_name")
+    @Column(name="username")
+    private String username;
+    @Column(name="password", length = 256)
+    private String password;
+    @Column(name="firstname")
     private String firstName;
-
-    @Size(min = 3, max = 15, message = "Tên cần trong khoảng 3-15 kí tự")
-    @Column(name = "last_name")
+    @Column(name="lastname")
     private String lastName;
 
-    private String username;
-
-    private String password;
-
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "user_id"),
-    inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "role_id"))
+    @JoinTable(
+            name="users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name="role_id")
+    )
     private Collection<Roles> roles;
 
+    public Users() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Collection<Roles> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Roles> roles) {
+        this.roles = roles;
+    }
+
+    public Users(Long id, String username, String password, String firstName, String lastName, Collection<Roles> roles) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.roles = roles;
+    }
+
+    public Users(String username, String password, String firstName, String lastName, Collection<Roles> roles) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
 }
