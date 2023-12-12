d2c4c36f8e838492f51081fb0f2a12113a1d4866
Nguyen Minh An <annm01032002@gmail.com>
2023-12-13 03:40:48 +0700

    Reconfig spring security

--- zilliqa_project/src/main/java/com/annm/zilliqa_project/config/SecurityConfig.java
+++ zilliqa_project/src/main/java/com/annm/zilliqa_project/config/SecurityConfig.java
@@ -4,44 +4,60 @@ import com.annm.zilliqa_project.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new UserServiceConfig();
+    }
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public DaoAuthenticationProvider authenticationProvider(UserService userService){
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(userService);
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-        return daoAuthenticationProvider;
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(
-                configurer->configurer
-                        .antMatchers("/*").permitAll()
-                        .antMatchers("/login/**").hasAnyRole("USER", "ADMIN")
-                        .anyRequest().authenticated()
-        ).formLogin(
-                form->form.loginPage("/login/showLoginPage")
-                        .loginProcessingUrl("/authenticateTheUser")
-                        .permitAll()
-        ).logout(
-                logout->logout.permitAll()
-        ).exceptionHandling(
-                configurer->configurer.accessDeniedPage("/showPage404")
-        );
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
 
-        return http.build();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/*").permitAll()
+                .antMatchers("/admin/*")
+                .hasAuthority("ADMIN")
+                .antMatchers("/user/*")
+                .hasAnyAuthority("ADMIN", "USER")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/admin/index")
+                .permitAll()
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .permitAll();
     }
 }
