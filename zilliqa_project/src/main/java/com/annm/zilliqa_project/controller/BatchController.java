fb46c0a7948d94b34646359edd283feefb8bbaf0
KawaiiHasuu <annm01032002@gmail.com>
2023-12-29 11:12:03 +0700

    Job restart

--- zilliqa_project/src/main/java/com/annm/zilliqa_project/controller/BatchController.java
+++ zilliqa_project/src/main/java/com/annm/zilliqa_project/controller/BatchController.java
@@ -2,11 +2,14 @@ package com.annm.zilliqa_project.controller;
 
 import com.annm.zilliqa_project.repository.ExceptionRepository;
 import com.annm.zilliqa_project.repository.TransactionRepository;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameter;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.JobLocator;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.NoSuchJobException;
+import org.springframework.batch.core.launch.NoSuchJobExecutionException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.batch.operations.JobRestartException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,9 +45,16 @@ public class BatchController {
     private Job runJob3;
 
     @Autowired
+    private JobOperator jobOperator;
+
+    @Autowired
+    private JobExecution execution;
+
+    @Autowired
     private JobLauncher jobLauncherAsync;
 
-    public static void runJob(JobLauncher jobLauncher, Job job) {
+    public static void runJob(JobLauncher jobLauncher, Job job) throws JobInstanceAlreadyCompleteException, NoSuchJobException, NoSuchJobExecutionException, JobParametersInvalidException, org.springframework.batch.core.repository.JobRestartException {
+        JobExecution jobExecution = null;
         JobParameters jobParameters = new JobParametersBuilder()
                 .addLong("startAt", System.currentTimeMillis()).toJobParameters();
         try {
@@ -51,12 +62,22 @@ public class BatchController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        JobOperator jobOperator1 = null;
+        jobOperator1.restart(jobExecution.getId());
+    }
+
+    public void reloadJob(long jobId) throws JobInstanceAlreadyCompleteException,
+            NoSuchJobExecutionException,
+            NoSuchJobException,
+            JobRestartException,
+            JobParametersInvalidException, org.springframework.batch.core.repository.JobRestartException {
+        jobOperator.restart(jobId);
     }
     @GetMapping("/run-batch")
     public String goBatch(){
         exceptionRepository.deleteAll();
         transactionRepository.deleteAll();
-//        runJob(jobLauncherAsync, runJob1);
+        runJob(jobLauncherAsync, runJob1);
         runJob(jobLauncherAsync, runJob2);
         runJob(jobLauncherAsync, runJob3);
         return "redirect:/home";
